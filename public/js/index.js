24e69762bfcf6e521a6f7c810a99987c1b773f21
GitProdEnv <kopfhoererimtest@web.de>
2018-05-05 19:09:20 +0200

    Add geolocation support via geolocation api

--- public/js/index.js
+++ public/js/index.js
@@ -26,6 +26,17 @@ socket.on('newMessage', function (message) {
     jQuery('#messages').append(li);
 });
 
+socket.on('newLocationMessage', function (message) {
+    var li = jQuery('<li></li>');
+    var a = jQuery('<a target="_blank">My current location</a>');
+
+    li.text(`${message.from}: `); // Not adding anfällig code in simple template strings, because there could be stored malicious code. We use here .text or .attr
+    a.attr('href', message.url);
+
+    li.append(a);
+    jQuery('#messages').append(li);
+});
+
 jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
 
@@ -34,5 +45,21 @@ jQuery('#message-form').on('submit', function (e) {
         text: jQuery('[name=message]').val()
     }, function () {  // Acknowledgment If some Error from client to server the server can pass back "OK" status or error status
 
+    });
+});
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function() {
+    if (!navigator.geolocation) {
+        return alert('Geolocation not supported by your browser.');
+    }
+
+    navigator.geolocation.getCurrentPosition(function (position) {
+        socket.emit('createLocationMessage', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        });
+    }, function() {
+        alert('Unable to fetch location.');
     });
 });
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
     it('should generate correct message object', () => {
@@ -10,4 +10,20 @@ describe('generateMessage', () => {
         expect(typeof message.createdAt).toBe('number');
         expect(message).toMatchObject({from, text});
     });
+});
+
+describe('generateLocation', () => {
+
+    it('should generate correct location object', () => {
+        const from = 'Adam';
+        const latitude = 12;
+        const longitude = 33;
+        const expectUrl = 'https://www.google.com/maps?q=12,33';
+
+        const message = generateLocationMessage(from, latitude, longitude);
+
+        expect(message).toMatchObject({from, url: expectUrl});
+        expect(typeof message.createdAt).toBe('number');
+    });
+
 });

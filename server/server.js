f682abc46cd2078489fb50a4c574fd48b7dab8bc
GitProdEnv <kopfhoererimtest@web.de>
2018-05-05 18:27:26 +0200

    Add form for messages and show incoming message

--- server/server.js
+++ server/server.js
@@ -22,10 +22,10 @@ io.on('connection', (socket) => {
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
     // Custom EventListener - Event that you want to listen to
-    socket.on('createMessage', (message) => {
+    socket.on('createMessage', (message, callback) => {
         console.log('createMessage', message);
         io.emit('newMessage', generateMessage(message.from, message.text));  // socket.emit emits an event to a single connection, io.emit emits an event to every single connection
-
+        callback('This is from the server.');
         // socket.broadcast.emit('newMessage', {
         //     from: message.from,
         //     text: message.text,
